a0dfbc77693fee9f2a0dcd8c8070bf3ee835c240
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-02-13 19:08:05 -0300

    Update demons.queries.js
    Updated some queries to make them more reusable.

--- src/queries/demons.queries.js
+++ src/queries/demons.queries.js
@@ -1,3 +1,4 @@
+/** Select all in database*/
 exports.get_all_demons = "SELECT * FROM demons";
-exports.get_demon_by_name = "SELECT * FROM demons WHERE name ILIKE '%' || $1 || '%'";
-exports.get_demon_by_lvl = "SELECT * FROM demons WHERE name ILIKE '%' || $1 || '%'";
+/** Select all where query passed from user (like 'name') is something like a query passed from user ('like odin') */
+exports.get_demon_by_query = "SELECT * FROM demons WHERE $1 ILIKE '%' || $2 || '%'";
