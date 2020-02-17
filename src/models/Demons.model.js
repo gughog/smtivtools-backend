c5e9674a9218f99b395a45c44d50198ae9e699cc
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-02-17 12:54:39 -0300

    getAll models draft finished

--- src/models/Demons.model.js
+++ src/models/Demons.model.js
@@ -1,13 +1,31 @@
-const Demons = {
+const { get_all_demons, get_demon_by_query } = require('../queries/demons.queries');
+
+const DemonsModels = {
   async getAll(req, res, connection) {
     const { name, level, resist, isweak, repeal, drain, isnull, ...unknown } = req.query;
 
     try {
-      
-    } catch (error) {
-      throw error;
-    } finally {
+      const results = await connection.query(get_all_demons)
 
+      if (results) {
+        res.status(200).json({
+          status: 'success',
+          message: null,
+          records: results.rows.length,
+          data: results.rows
+        })
+      }
+
+    } catch (error) {
+      res.status(404).send({
+        status: 'error',
+        message: 'Some error has occured.',
+        records: 0,
+        error: true
+      });
+
+    } finally {
+      console.log('END OF REQUEST')
     }
   },
 
@@ -16,4 +34,6 @@ const Demons = {
   }
 
   //TODO: getById () {}
-}
+};
+
+module.exports = DemonsModels;
