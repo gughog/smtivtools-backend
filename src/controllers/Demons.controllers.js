b1db7f095229d9853f67eea1472497163e069523
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-02-18 18:54:38 -0300

    Added first functional route request.

--- src/controllers/Demons.controllers.js
+++ src/controllers/Demons.controllers.js
@@ -1,6 +1,8 @@
-const { get_all_demons, get_demon_by_query } = require('../queries/demons.queries');
+const { queryGenerator } = require('../utils/index');
 
-const DemonsModels = {
+const { get_all_demons } = require('../queries/demons.queries');
+
+const DemonsController = {
   async getAll(req, res, connection) {
     try {
       const results = await connection.query(get_all_demons);
@@ -19,7 +21,7 @@ const DemonsModels = {
       // If has errors, then return 500 (internal error code);
       res.status(500).json({
         status: 'error',
-        message: 'Some internal error has occured.',
+        message: error,
         records: 0,
         error: true
       });
@@ -28,33 +30,40 @@ const DemonsModels = {
     }
   },
 
-  async searchBy(req, res, connection) {
-    const { name, level, race, resist, isweak, repeal, drain, isnull, ...unknown } = req.query;
-    const params = [ name, level, race, resist, isweak, repeal, drain, isnull ];
+  // TODO:
+  // async searchBy(req, res, connection) {
+  //   const { name, level, race, resist, isweak, repeal, drain, isnull, ...unknown } = req.query;
+  //   const params = [ name, level, race, resist, isweak, repeal, drain, isnull ];
 
-    try {
-      // If some unknown queries is passed, returns an error
-      if (unknown && unknown.length > 0) {
-        res.status(404).json({
-          status: 'not_found',
-          message: `The following request queries are unknown: ${unknown.join()}.`,
-          records: 0,
-          error: true
-        });
-      }
+  //   try {
+  //     // If some unknown queries is passed, returns an error
+  //     if (unknown && unknown.length > 0) {
+  //       res.status(404).json({
+  //         status: 'not_found',
+  //         message: `The following request queries are unknown: ${unknown.join()}.`,
+  //         records: 0,
+  //         error: true
+  //       });
+  //     }
       
-      // Case queries are ok, then try to connect:
-      const results = await connection.query(get_all_demons, `SELECT * demons WHERE ${fields.map((i, idx) => {
-        return `${i} = $${idx + 1}${idx+1 === fields.length ? '' : ' AND'}`
-      }).join(' ')}`);
+  //     // Case queries are ok, then try to connect:
+  //     const results = await connection.query(queryGenerator('demons', params));
 
-    } catch (error) {
+  //     if (results) {
+  //       res.status(200).json({
+  //         status: 'success',
+  //         message: null,
+  //         records: results.rows.length,
+  //         data: results.rows
+  //       })
+  //     }
+  //   } catch (error) {
 
-    } finally {
-    }
-  }
+  //   } finally {
+  //   }
+  // }
 
   //TODO: getById () {}
 };
 
-module.exports = DemonsModels;
+module.exports = DemonsController;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,12 @@
 // Import all controllers here.
+const DemonsController = require('../controllers/Demons.controllers');
 
 const all_routes = (app, connection) => {
   app.get('/', (req, res) => {
     res.send('<h1>HEEEE HOOOO!</h1>')
     console.log('(!) "Hee hoo" route was requested.')
   })
-  app.get('/demons', /* controller goes here */)
+  app.get('/demons', async (req, res) => await DemonsController.getAll(req, res, connection) /* controller goes here */)
   app.get('/skills', /* controller goes here */)
   app.get('/apps', /* controller goes here */)
   app.get('/specialfusions', /* controller goes here */)
