4c739e280e907af5dadebee200a9fdbb97037511
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-02-20 13:27:43 -0300

    :warning: Hot fixes.

--- src/queries/demons.queries.js
+++ src/queries/demons.queries.js
@@ -2,3 +2,6 @@
 exports.getAllDemons = 'SELECT * FROM demons';
 exports.getDemonById = 'SELECT * FROM demons WHERE id = $1';
 exports.getRandomDemon = 'SELECT * FROM demons ORDER BY random() LIMIT $1';
+
+// "ORDER BY random() LIMIT $1" may have some performance penalties on larger
+// tables, but all tables from this db isn't, so It may be ok.
