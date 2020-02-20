e40915735af405f6c6b7ea5fad84b17d698adff7
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-02-20 09:56:59 -0300

    :fire: Fix and feature added: Now queries are case insensitive for textual values and numbers are normal queries, so, if we search on /api/v1/demons?name=pixie (or even pixi) it will find even if "Pixie" is the value registered on database.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,28 @@
 /**
+ * A wrapper for the element being iterated. If it is a textual
+ * element will be formatted to an 'ILIKE % $1 %' search query to be
+ * executed by db. If is a number, will be a normal WHERE clausule,
+ * 'WHERE x = y'.
+ *
+ * @param {*} element - Element to be evaluated and wrapped.
+ * @param {*} index - Index of this element, to generate the named parameter
+ * for postgres, like '$1'.
+ */
+const dataWrapper = (element, index) => {
+  const elementCopy = Object.keys(element);
+  const elementValueCopy = Object.values(element)[0];
+
+  // If elementValueCopy IS a number returns a normal WHERE:
+  if (!Number.isNaN(Number(elementValueCopy))) {
+    console.log(elementValueCopy, 'IS A NUMBER VALUE');
+    return `${elementCopy} = $${index + 1}`;
+  }
+
+  // Else, return a textual insensitive-case like search on WHERE:
+  return `${elementCopy} ILIKE '%' || $${index + 1} || '%'`;
+};
+
+/**
  * A function to write a simple sql query for multiples 'AND' clausules.
  * It has some very basic sql injection preventions, that throws an error
  * if something is caught.
@@ -29,7 +53,7 @@ exports.queryGenerator = async (table, fields) => {
   if (fieldsCopy && fieldsCopy.length < 1) {
     result = [];
   } else {
-    result = `SELECT * from ${table} WHERE ${fieldsCopy.map((i, idx) => `${Object.keys(i)} = $${idx + 1}${idx + 1 === fieldsCopy.length ? ';' : ' AND'}`).join(' ')}`;
+    result = `SELECT * from ${table} WHERE ${fieldsCopy.map((i, idx) => `${dataWrapper(i, idx)}${idx + 1 === fieldsCopy.length ? ';' : ' AND'}`).join(' ')}`;
   }
 
   return result;
